feacc51fc95a52a64a47f30cd126e00eda493854
Julien Esposito <julien.esposito@gmail.com>
2016-07-07 13:40:28 -0700

    Chapter 2 - Assignment - Starter
    This is a “getting started” code for Week 2 (Chapter 2 ) assignment.

---
+++ ccsd_2016_mod07/create_movie.sql
@@ -0,0 +1,38 @@
+-- 1.a
+create table Director
+	(
+		dir_num decimal(4,0) primary key,
+		name varchar(50) not null,
+		year_born decimal(4,0) not null,
+		year_death decimal(4,0) null
+
+	);
+
+-- 1.b
+create table Movie
+	(
+		movie_number decimal( 4, 0 ) primary key,
+		title varchar( 30 ) not null,
+		year_created decimal(4,0) not null,
+		type varchar( 15 ) not null,
+		dir_num decimal(4,0) not null,
+		critics_id decimal( 4, 0 ) not null
+	);
+
+
+create table Critics
+	(
+		critics_id decimal( 4, 0 ) primary key,
+		critics_rating decimal(2,1) not null,
+		mpaa_rating char( 4 ) not null,
+		number_awards_nom decimal( 2, 0 ) not null,
+		number_awards_won decimal( 2, 0 ) not null
+		);
+
+
+-- Question 2
+--------------
+-- +-----------+          +------------+
+-- |           | 1      oo|            |
+-- | Director  | -------> | Movie      |
+-- +-----------| (directs)+------------+
